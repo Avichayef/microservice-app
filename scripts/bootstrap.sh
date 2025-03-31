a96e334913289bccf2722307636406852f1b482f
avichayef <avichayef@newserver4.myguest.virtualbox.org>
2025-03-31 11:39:36 +0300

    fix9

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -5,19 +5,22 @@ set -e
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 PROJECT_ROOT="$( cd "$SCRIPT_DIR/.." && pwd )"
 
-echo "Starting bootstrap process..."
+echo "Current directory: $(pwd)"
+echo "Script directory: $SCRIPT_DIR"
+echo "Project root: $PROJECT_ROOT"
+echo "Terraform version: $(terraform version)"
 
-# Get GitHub repository information from git config
+# Get GitHub repository information
 GITHUB_REPO_URL=$(git config --get remote.origin.url)
 GITHUB_ORG=$(echo $GITHUB_REPO_URL | sed -n 's/.*github.com[:/]\([^/]*\).*/\1/p')
 GITHUB_REPO=$(echo $GITHUB_REPO_URL | sed -n 's/.*github.com[:/][^/]*\/\([^.]*\).*/\1/p')
 
-# Run bootstrap Terraform
+echo "Running Terraform bootstrap..."
 cd "$PROJECT_ROOT/terraform/bootstrap"
+echo "Changed to directory: $(pwd)"
 
-# Use the full path to terraform
-/usr/local/bin/terraform init
-/usr/local/bin/terraform apply -auto-approve \
+terraform init
+terraform apply -auto-approve \
   -var="github_org=$GITHUB_ORG" \
   -var="github_repo=$GITHUB_REPO"
 
